b77f5d5270fd78e9e250c40dfbc06ff07c91bfc1
Marek Fišera <mara@neptuo.com>
2026-03-05 11:51:12 +0100

    Today better filtering

--- src/TodoExtended.Web/Services/GraphTodoService.cs
+++ src/TodoExtended.Web/Services/GraphTodoService.cs
@@ -35,20 +35,24 @@ public class GraphTodoService(GraphServiceClient graphClient) : ITodoService
     public async Task<IReadOnlyList<TodoTaskWithList>> GetTodayTasksAsync()
     {
         var lists = await GetTaskListsAsync();
-        var today = DateOnly.FromDateTime(DateTime.Today);
+        var today = DateTime.Today;
+        var tomorrow = today.AddDays(1);
+        var filter = $"dueDateTime/dateTime ge '{today:yyyy-MM-dd}T00:00:00' and dueDateTime/dateTime lt '{tomorrow:yyyy-MM-dd}T00:00:00'";
         var result = new List<TodoTaskWithList>();
 
         foreach (var list in lists)
         {
-            var response = await graphClient.Me.Todo.Lists[list.Id].Tasks.GetAsync();
+            var response = await graphClient.Me.Todo.Lists[list.Id].Tasks.GetAsync(config =>
+            {
+                config.QueryParameters.Filter = filter;
+            });
             if (response?.Value is null) continue;
 
             foreach (var t in response.Value)
             {
-                if (t.DueDateTime?.DateTime is null) continue;
-
-                var due = DateTimeOffset.Parse(t.DueDateTime.DateTime);
-                if (DateOnly.FromDateTime(due.Date) != today) continue;
+                var due = t.DueDateTime?.DateTime is not null
+                    ? DateTimeOffset.Parse(t.DueDateTime.DateTime)
+                    : (DateTimeOffset?)null;
 
                 result.Add(new TodoTaskWithList(
                     t.Id!,
